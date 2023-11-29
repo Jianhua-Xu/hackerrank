6ae18b96faa67194d9518ba638642c898454ec5c
Jianhua Xu <jianhuaxu@outlook.com>
2023-11-29 14:11:29 -0500

    feat: add explanations for most_common(n)
    not working easily in this case as explained in the script

--- python/collections_company_logo/company_logo.py
+++ python/collections_company_logo/company_logo.py
@@ -14,3 +14,7 @@ if __name__ == '__main__':
     for k, v in itertools.islice(sorted(d.items(), key=lambda x: (-x[1], x[0])), 3):
         print(k, v)
    
+   ## NOTE
+   # we can use Counter.most_common(3) to retrieve the highest 3, but, when there are equal counts,
+   # things could get hard. For example, we have one count 3, but 5 count 2s. We need to get 3,2,2, 
+   # but which 2 to pick? Methods most_commons cannot handle this 
